9dc365cf261a5380352f93373f404484c30aa083
Vincent DE BAERE <vdebaere@openlab.be>
2020-05-17 13:45:18 +0200

    Add support for shell and privileged shell actions

--- zvault/action.py
+++ zvault/action.py
@@ -1,12 +1,16 @@
 import logging
+import subprocess
+import typing
 
 
 class Result:
 
     OK: bool
+    exception: BaseException
 
-    def __init__(self, success=True):
+    def __init__(self, success=True, exception=None):
         self.OK = success
+        self.exception = exception
 
 
 class Action:
@@ -18,11 +22,53 @@ class Action:
         self.result = None
         self.rollback_result = None
 
-    def invoke(self) -> None:
+    def _pre_invoke(self, context: dict) -> None:
         pass
 
-    def rollback(self) -> None:
+    def invoke(self, context: dict) -> None:
         pass
+
+    def _pre_rollback(self, context: dict) -> None:
+        pass
+
+    def rollback(self, context: dict) -> None:
+        pass
+
+
+def pkexec(orig_class):
+
+    _build_command_orig = orig_class._build_command
+    _build_rollback_command_orig = orig_class._build_rollback_command
+
+    def _build_command_pkexec():
+        return _build_command_orig().insert(0, 'pkexec')
+
+    def _build_rollback_command_pkexec():
+        return _build_rollback_command_orig().insert(0, 'pkexec')
+
+    orig_class._build_command = _build_command_pkexec
+    orig_class._build_rollback_command = _build_rollback_command_pkexec
+
+
+class ShellAction(Action):
+
+    def __init__(self):
+        super().__init__()
+
+    def invoke(self, context: dict) -> None:
+        self._pre_invoke()
+        cp = subprocess.run(self._build_command(), shell=True)
+        try:
+            cp.check_returncode()
+            self.result = Result()
+        except subprocess.CalledProcessError as e:
+            self.result = Result(False, e)
+
+    def _build_invoke_command(self) -> typing.List[str]:
+        return []
+
+    def _build_rollback_command(self) -> typing.List[str]:
+        return []
 
 
 class LogAction(Action):
@@ -31,6 +77,6 @@ class LogAction(Action):
         super().__init__()
         self._message = message
 
-    def invoke(self) -> None:
+    def invoke(self, context: dict) -> None:
         logging.info(self._message)
         self.result = Result()
